d21220c4009d449ccd6ec7d4b63f929e92152532
VIP_IN <vipkum05@gmail.com>
2026-02-22 14:25:31 +0530

    Update README.bash

--- README.bash
+++ README.bash
@@ -1,5 +1,6 @@
 #Install requirements:
-
+python -m venv venv
+source venv/bin/activate
 pip install pyautogui pyinstaller
 
 pyinstaller --onefile --noconsole --name "SystemUpdate.exe" mouse_jitter.py
