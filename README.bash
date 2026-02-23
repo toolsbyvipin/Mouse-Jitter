9c3454f30071e30e198687e93582372e0b2ec658
VIP_IN <vipkum05@gmail.com>
2026-02-23 17:32:05 +0530

    Update README.bash

--- README.bash
+++ README.bash
@@ -1,3 +1,10 @@
+#No file needed
+
+CHECK One-Liner-Jitter 
+One-Liner-Freezer
+
+
+
 #Install requirements:
 python -m venv venv
 source venv/bin/activate
@@ -5,5 +12,4 @@ pip install pyautogui pyinstaller
 
 pyinstaller --onefile --noconsole --name "SystemUpdate.exe" mouse_jitter.py
 
-jitter_duration = 60  # Increase to 1 minute
-jitter_intensity = 25  # More aggressive movement
+
